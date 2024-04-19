81106579f7e92e233691aa30064b35f65a113b66
abdessamadsalim <samimalim1991@gmail.com>
2024-04-19 09:35:22 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,11 @@
-# install flask from pip3
+# 1-install_a_package.pp
 
-# Install Flask using pip3
-package { 'Flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
 
-# Install Werkzeug using pip3
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
