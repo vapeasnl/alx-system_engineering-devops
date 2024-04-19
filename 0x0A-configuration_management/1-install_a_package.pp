be1c260282dcb0bfa86e0b64023a8d52550d2e29
abdessamadsalim <samimalim1991@gmail.com>
2024-04-19 09:41:14 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# how to install a package using puppet
+# install flask from pip3
 
 package { 'flask':
-  ensure => "installed"
-  }
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
+}
+
+# Install Werkzeug using pip3
+package { 'Werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip3',
+}
+
