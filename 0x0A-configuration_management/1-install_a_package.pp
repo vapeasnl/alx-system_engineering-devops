2819a6217677324d81f3008f693c426ea6bdb9ba
abdessamadsalim <samimalim1991@gmail.com>
2024-04-19 11:23:21 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-#!/usr/bin/pup
-
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
-# Ensure python3-pip is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
