a52357393a82847ef3e110e1d91754e807de0848
abdessamadsalim <samimalim1991@gmail.com>
2024-05-20 01:21:35 -0700

    task 2 done

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Using what i did in the task #0
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
+    url = jsonplaceholder + '/' + USER_ID
+    response = requests.get(url)
+    username = response.json().get('username')
+    todo_url = url + '/todos'
+    response = requests.get(todo_url)
+    tasks = response.json()
+    dict = {USER_ID: []}
+    for task in tasks:
+        dict[USER_ID].append({
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username
+        })
+        with open('{}.json'.format(USER_ID), 'w') as f:
+            json.dump(dict, f)
