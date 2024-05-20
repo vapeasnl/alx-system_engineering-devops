cb1e1d5ca620af14683622acefc4aa46395735ee
abdessamadsalim <samimalim1991@gmail.com>
2024-05-20 09:02:18 -0700

    upd1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,57 +1,33 @@
 #!/usr/bin/python3
 """
-    Task api 1
+    Task 1
 """
-
 import csv
 import requests
-import sys import argv
-
-
-def export_employee_todo_csv(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        # Retrieve employee information
-        response = requests.get(employee_url)
-        response.raise_for_status()
-        employee_data = response.json()
-
-        # Retrieve employee's TODO list
-        response = requests.get(todos_url)
-        response.raise_for_status()
-        todos_data = response.json()
-
-        # Extract relevant information
-        user_id = employee_data['id']
-        username = employee_data['username']
-
-        # Prepare CSV file name
-        file_name = f"{user_id}.csv"
-
-        # Prepare data for CSV export
-        csv_data = []
-        for todo in todos_data:
-            task_completed_status = "True" \
-                if todo['completed'] else "False"
-            task_title = todo['title']
-            csv_data.append([
-                user_id, username, task_completed_status, task_title])
-
-        # Export data to CSV file
-        with open(file_name, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            writer.writerows(csv_data)
-
-        print(f"CSV file '{file_name}' exported successfully.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {str(e)}")
-        return
-
+from sys import argv
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    export_employee_todo_csv(employee_id)
+    # Check if the script is provided with an employee ID as a command-line argument
+    if len(argv) != 2:
+        print("Usage: ./1-export_to_CSV.py <employee_id>")
+        exit(1)
+
+    employee_id = argv[1]
+
+    rusr = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    rtodo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+
+    jusr = rusr.json()
+    jtodo = rtodo.json()
+
+    name = jusr.get('username')
+    fname = '{}.csv'.format(employee_id)
+
+    with open(fname, mode='w') as user_file:
+        user_writer = csv.writer(user_file, delimiter=',', quotechar='"',
+                                 quoting=csv.QUOTE_ALL)
+        for t in jtodo:
+            user_writer.writerow([employee_id, name, t.get('completed'),
+                                  t.get('title')])
