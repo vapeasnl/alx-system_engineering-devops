8bd4488225b232850a0e829042c3988f156f9807
abdessamadsalim <samimalim1991@gmail.com>
2024-06-10 02:35:47 -0700

    task 1 and 2

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+
+# Change the OS configuration so that it is possible to login with the
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
+
